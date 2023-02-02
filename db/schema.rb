77e248f39e85c783502f73b48e486ca48de183cb
山田怜央 <yamadareo@yamadareiounoMacBook-ea.local>
2023-02-02 19:04:45 +0900

    お知らせ新規投稿機能の追加
    お知らせ一覧ページの作成
    お知らせ新規ページの作成
    arriclesテーブル（お知らせ）作成
    お知らせに関するルートの設定
    お知らせタイトルのバリデーション追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_01_103621) do
+ActiveRecord::Schema.define(version: 2023_02_01_131821) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "departments", force: :cascade do |t|
     t.string "name", null: false
